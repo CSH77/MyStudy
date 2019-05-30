071a3218c256ae06ac7ed2c8339bb55afc3860c0
SeungHwan <seunghwanchoi@outlook.com>
2019-05-30 07:57:19 -0600

    solution handle single negatgive value

--- main.cpp
+++ main.cpp
@@ -6,27 +6,25 @@ using namespace std;
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int maxHere = 0;
-        int maxEntire = 0;
+        int currentSum = INT32_MIN;
+        int maxSum = INT32_MIN;
 
         for(int i = 0; i < nums.size(); i++)
         {
-            maxHere = maxHere + nums[i];
-            if(maxHere < 0)
-                maxHere = 0;
-            else if(maxHere > maxEntire)
-                maxEntire = maxHere;    
+            currentSum = max(currentSum, 0) + nums[i];
+            maxSum = max(maxSum, currentSum);
         }    
     
-        return maxEntire;
+        return maxSum;
     }
 };
 
 int main()
 {
     //[4,-1,2,1] has the largest sum = 6.
-    vector<int> input = {-2,1,-3,4,-1,2,1,-5,4}; //output 6
+    //vector<int> input = {-2,1,-3,4,-1,2,1,-5,4}; //output 6
 
+    vector<int> input = {-1};
     Solution obj;
     cout << obj.maxSubArray(input);
 
