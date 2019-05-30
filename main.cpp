920dd6ba4fc415c1ddbc7df99587d58a050533dd
SeungHwan <seunghwanchoi@outlook.com>
2019-05-29 22:43:41 -0600

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
-    
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+
+    }
+};
+
 int main()
 {
-    cout << "hello world!" << endl;
-        
+    //[4,-1,2,1] has the largest sum = 6.
+    vector<int> input = {-2,1,-3,4,-1,2,1,-5,4}; //output 6
+
+
     return 0;
 }
 
