de68916c520b80d2012502743efb02761085eeed
SeungHwan <seunghwanchoi@outlook.com>
2019-05-31 23:20:54 -0600

    house robber dynamic programming

--- main.cpp
+++ main.cpp
@@ -1,11 +1,30 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
-    
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int currentMax = 0;
+        int prevMax = 0;
+        for(int x : nums)
+        {
+            int temp = currentMax;
+            currentMax = max(prevMax + x, currentMax);
+            prevMax = temp;
+        }
+        return currentMax;
+    }
+};
+
 int main()
 {
-    cout << "hello world!" << endl;
-        
+    //vector<int> input = {1,2,3,1}; //expected output 4;
+    vector<int> input = {2,7,9,3,1}; //expected output 12;
+    Solution obj;
+    cout << obj.rob(input);
+
     return 0;
 }
 
