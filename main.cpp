0f8406d7706a2ee0c1f38d7495ba9aa898e63d2d
SeungHwan <seunghwanchoi@outlook.com>
2019-06-04 23:58:00 -0600

    shuffle_vector

--- main.cpp
+++ main.cpp
@@ -1,13 +1,39 @@
 #include <iostream>
+#include <vector>
+#include <random>
+#include <functional>
+#include <chrono>
 
 using namespace std;
-    
+
+class Solution
+{
+    public:
+        void shuffle(vector<int>& vec)
+        {
+            for(int i = vec.size() - 1; i >= 0; i--)
+            {
+                unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+                std::default_random_engine generator(seed);
+                std::uniform_int_distribution<int> distribution(1, vec.size());
+                int j = distribution(generator);
+
+                //int diceRoll = distribution(generator);
+                swap(vec[i], vec[j]);
+            }
+        }
+
+};
+
 int main()
 {
-    cout << "hello world!" << endl;
-        
+    vector<int> input = {1,2,3,4,5,6,7};
+    Solution obj;
+    obj.shuffle(input);
+
+    for(int n : input)
+        cout << n << " ";
+    cout << endl;
+
     return 0;
 }
-
-
-
