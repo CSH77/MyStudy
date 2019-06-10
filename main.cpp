ae917660ec688f865e15e5baccd8905360a68b6f
SeungHwan <seunghwanchoi@outlook.com>
2019-06-09 22:08:41 -0600

    prime number count

--- main.cpp
+++ main.cpp
@@ -1,11 +1,37 @@
 #include <iostream>
 
 using namespace std;
-    
+
+class Solution {
+public:
+    bool isPrime(int n)
+    {
+        for(int i = 2; i <= n / 2; i++)
+        {
+            if( n % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    int countPrimes(int n) {
+        int totalPrimeCount = 0;
+        for(int i = 2; i <= n; i++)
+        {
+            if( isPrime(i) )
+                totalPrimeCount++;
+        }
+
+        return totalPrimeCount;
+    }
+};
+
 int main()
 {
-    cout << "hello world!" << endl;
-        
+    Solution obj;
+    // cout << obj.isPrime(4) << endl;
+    cout << obj.countPrimes(10) << endl;
+
     return 0;
 }
 
