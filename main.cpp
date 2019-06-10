885cf1d39381bc534b23c35eea9217449e010ada
SeungHwan <seunghwanchoi@outlook.com>
2019-06-09 22:27:30 -0600

    primeNumber_faster_solution

--- main.cpp
+++ main.cpp
@@ -6,12 +6,21 @@ class Solution {
 public:
     bool isPrime(int n)
     {
-        for(int i = 2; i <= n / 2; i++)
+        if( n <= 1)
+            return false;
+        if(n <= 3)
+            return true;
+
+        if( n % 2 == 0 || n % 3 == 0)
+            return false;
+
+        for(int i = 5; i * i <= n; i = i + 6)
         {
-            if( n % i == 0)
+            if( n % i == 0 || n % (i + 2) == 0)
                 return false;
         }
         return true;
+
     }
 
     int countPrimes(int n) {
@@ -29,8 +38,8 @@ public:
 int main()
 {
     Solution obj;
-    // cout << obj.isPrime(4) << endl;
-    cout << obj.countPrimes(10) << endl;
+    cout << obj.isPrime(4997) << endl;
+    // cout << obj.countPrimes(10) << endl;
 
     return 0;
 }
