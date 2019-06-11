2bc2298fb58224fdfd0c24fe30e042f6deca5da5
SeungHwan <seunghwanchoi@outlook.com>
2019-06-10 22:57:48 -0600

    bitCount

--- main.cpp
+++ main.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
-    
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+
+        int count = 0;
+        while(n)
+        {
+            count = count + (n & 1);
+            n = n >> 1;
+        }
+        return count;
+    }
+};
+
 int main()
 {
-    cout << "hello world!" << endl;
-        
+    uint32_t num = 11; //expect 3
+
+    Solution obj;
+    cout << obj.hammingWeight(num);
+
     return 0;
 }
 
