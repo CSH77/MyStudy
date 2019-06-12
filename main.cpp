f07340e4a02df8a2fa5095355d011ee00dcb9a7b
SeungHwan <seunghwanchoi@outlook.com>
2019-06-11 22:28:18 -0600

    hamming distance, find total number of difference  bit

--- main.cpp
+++ main.cpp
@@ -1,11 +1,34 @@
 #include <iostream>
 
 using namespace std;
-    
+
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int count = 0;
+
+        //xor
+        int xorNum = x ^ y;
+
+        while( xorNum != 0 )
+        {
+            if( xorNum & 1 )
+            {
+                count++;
+            }
+            xorNum = xorNum >> 1;
+        }
+        return count;
+    }
+};
+
+
 int main()
 {
-    cout << "hello world!" << endl;
-        
+    Solution obj;
+    //cout << obj.hammingDistance(1,4) << endl;
+    cout << obj.hammingDistance(93,73) << endl;
+
     return 0;
 }
 
