7d89c3fb3855fb7832f99ac25bcfc1f9c0f76e9e
SeungHwan <seunghwanchoi@outlook.com>
2019-06-13 00:59:51 -0600

    find missing number

--- main.cpp
+++ main.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
-    
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int totalNum = 0;
+        int total = 0;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            totalNum = totalNum + nums[i];
+            total = total + (i + 1);
+        }
+        int missingNum = total - totalNum;
+
+        return missingNum;
+    }
+
+};
+
+
 int main()
 {
-    cout << "hello world!" << endl;
-        
+    Solution obj;
+    vector<int> input = {9,6,4,2,3,5,7,0,1};
+
+    cout << obj.missingNumber(input) << endl;
+
     return 0;
 }
-
-
-
