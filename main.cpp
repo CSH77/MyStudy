08e8e84ec42982458a17b578247658f1a3c54e66
SeungHwan <seunghwanchoi@outlook.com>
2019-06-19 21:07:38 -0600

    find single time number

--- main.cpp
+++ main.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
-    
+
+/* 
+Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+
+Note:
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+Example 1:
+Input: [2,2,1]
+Output: 1
+
+Example 2:
+Input: [4,1,2,1,2]
+Output: 4
+ */
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int total = 0;
+        for(int n : nums) 
+        {
+            total = total ^ n;
+        }      
+        return total;
+    }
+};
+
 int main()
 {
-    cout << "hello world!" << endl;
+    // vector<int> input = {2,2,1};
+    vector<int> input = {4,1,2,1,2};
+    Solution obj;
+    cout << obj.singleNumber(input) << endl;
         
     return 0;
 }
