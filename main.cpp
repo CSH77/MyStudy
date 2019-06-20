d117a0342936d70818594704ae8054b930902a73
SeungHwan <seunghwanchoi@outlook.com>
2019-06-19 23:22:26 -0600

    move zeroes

--- main.cpp
+++ main.cpp
@@ -2,11 +2,51 @@
 #include <vector>
 
 using namespace std;
-    
+/* 
+Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+Example:
+Input: [0,1,0,3,12]
+Output: [1,3,12,0,0]
+ */
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        
+        for(unsigned int i = 0; i < nums.size(); i++)
+        {
+            if(nums[i] == 0)
+            {
+                int nonZeroIndex = i;
+                while(nonZeroIndex < nums.size() - 1 )
+                {
+                   if(  nums[nonZeroIndex] != 0 )
+                   {
+                       break;
+                   }
+                   nonZeroIndex++;
+                }
+
+                nums[i] = nums[nonZeroIndex];
+                nums[nonZeroIndex] = 0;
+            }
+        }        
+        
+    }
+};
+
 int main()
 {
-    cout << "hello world!" << endl;
-        
+    vector<int> input = {0,1,0,3,12};
+    Solution obj;
+    obj.moveZeroes(input);
+
+    for(int n : input)
+    {
+        cout << n << " ";
+    }
+    cout << endl;
+
     return 0;
 }
 
