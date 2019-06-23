5ddf3c2ba3914a9719ec172290b5a23fedfe497a
SeungHwan <seunghwanchoi@outlook.com>
2019-06-23 01:55:28 -0600

    maximum depth of binary tree

--- main.cpp
+++ main.cpp
@@ -2,10 +2,54 @@
 #include <vector>
 
 using namespace std;
-    
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+
+        if(root == NULL)
+            return 0;
+
+        int leftDepth = maxDepth(root->left);
+        int rightDepth = maxDepth(root->right);
+
+        if(leftDepth > rightDepth)
+            return leftDepth + 1;
+        else 
+            return rightDepth + 1;
+    }
+};
+
+//Given binary tree [3,9,20,null,null,15,7],
+//     3
+//    / \
+//   9  20
+//     /  \
+//    15   7
+
 int main()
 {
-    cout << "hello world!" << endl;
+    TreeNode* node3 = new TreeNode(3);
+    TreeNode* node9 = new TreeNode(9);
+    TreeNode* node20 = new TreeNode(20);
+    TreeNode* node15 = new TreeNode(15);
+    TreeNode* node7 = new TreeNode(7);
+
+    node3->left = node9;
+    node3->right = node20;
+    node20->left = node15;
+    node20->right = node7;
+
+    Solution obj;
+    cout << obj.maxDepth(node3) << endl;
         
     return 0;
 }
